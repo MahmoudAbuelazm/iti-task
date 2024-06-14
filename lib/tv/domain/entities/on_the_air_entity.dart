8c110a786cbb147c371e674f00645750f1bbca9f
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-06-14 15:44:24 +0300

    Add OnTheAirEntity class and BaseMoviesRepository interface

---
+++ lib/tv/domain/entities/on_the_air_entity.dart
@@ -0,0 +1,9 @@
+class OnTheAirEntity {
+  final String backdropPath;
+  final String originalName;
+  final String overview;
+  final String voteAverage;
+
+  OnTheAirEntity(
+      this.backdropPath, this.originalName, this.overview, this.voteAverage);
+}
